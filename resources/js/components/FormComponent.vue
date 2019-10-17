e0e86236782c4ee2ac5e78863851984eb167e9ab
shu ichikawa <radio.radirer@gmail.com>
2019-10-17 10:53:09 +0900

    ツイートフォーム追加

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-md-4">
+        <form>
+            <div class="form-group">
+                <textarea 
+                    class="form-control" 
+                    cols="8" rows="8"
+                    maxlength="130"
+                    required>
+                </textarea>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary">
+                    Tweet
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4">
-            Tweet Form
-        </div>
+        <form-component></form-component>
         <div class="col-md-8">
             TimeLines
         </div>
